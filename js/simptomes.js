b74163d45ddcc224e6399686ea964c009bfc3aea
SKISTE <62730180+SKISTE@users.noreply.github.com>
2022-06-25 12:51:37 +0400

    Добавлена кнопка "Добавленные симптомы"

--- js/simptomes.js
+++ js/simptomes.js
@@ -1,3 +1,5 @@
+
+
 var input = document.getElementById('Simptomes_textarea');
 input.oninput = function() {
     for (var i = document.getElementsByClassName('simptome').length - 1; i >= 0; i--) {
@@ -11,3 +13,15 @@ input.oninput = function() {
       
     }
   };
+
+function AddSimptome(num){
+  console.log(simptomes_list[num])
+}
+function ShowPopup(){
+  document.getElementById('Added_simptomes_window').style.opacity = 1
+  document.getElementById('Added_simptomes_window').style.display = 'block'
+}
+function ClosePopup(){
+  document.getElementById('Added_simptomes_window').style.opacity = 0
+  document.getElementById('Added_simptomes_window').style.display = 'none'
+}
