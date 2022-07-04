ab3c71af78f81f2ecaae923af53d979f0366ebd9
SKISTE <62730180+SKISTE@users.noreply.github.com>
2022-07-04 15:30:22 +0400

    Анкета, ее автозаролнение

--- js/anketa.js
+++ js/anketa.js
@@ -7,3 +7,29 @@ function remove_anketa() {
 	document.getElementById('anketa').style.animation = 'remove_anketa .7s forwards'
 	//document.getElementById('button_anketa').setAttribute('onclick','show_anketa()')
 }
+
+function anketa_preload(){
+	try{
+		document.getElementById('anketa_age').value = age
+	}
+	catch (err){
+		CONSOLE_ERROR(err)
+	}
+	var date = new Date()
+	document.getElementById('anketa_year').value = date.getFullYear()
+	document.getElementById('anketa_day').value = date.getDate()
+	document.getElementById('anketa_month').value = (date.getMonth() == 0) ? 'Январь' :
+		(date.getMonth() == 1) ? 'Февраль' :
+		(date.getMonth() == 2) ? 'Март' :
+		(date.getMonth() == 3) ? 'Апрель' :
+		(date.getMonth() == 4) ? 'Май' :
+		(date.getMonth() == 5) ? 'Июнь' :
+		(date.getMonth() == 6) ? 'Июль' :
+		(date.getMonth() == 7) ? 'Август' :
+		(date.getMonth() == 8) ? 'Сентябрь' :
+		(date.getMonth() == 9) ? 'Октябрь' :
+		(date.getMonth() == 10) ? 'Ноябрь' :
+		(date.getMonth() == 11) ? 'Декабрь' :
+		'NaN'
+}
+anketa_preload()
--- js/onpreload.js
+++ js/onpreload.js
@@ -1,3 +1,6 @@
 console.groupCollapsed('Preload.js')
 console.log('ID for animals:\n1 - Собака\n2 - Кошка\n3 - Крупно рогатый скот\n4 - Мелко рогатый скот\n5 - Птица')
 console.groupEnd()
+function CONSOLE_ERROR(e) {
+	console.error(`${e.name} : ${e.message} : ${e.stack}`)
+}
